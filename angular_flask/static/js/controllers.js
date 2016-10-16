7f4f7cbb0c9d260d4142ec8f09dc86be982f586d
ian luddy <ianluddy@gmail.com>
2016-10-16 13:49:10 +0100

    Login/Signup

--- angular_flask/static/js/controllers.js
+++ angular_flask/static/js/controllers.js
@@ -23,7 +23,7 @@ function SearchController($scope, $routeParams, $location, Search) {
 
 	Search.get(args, function(result) {
 		$scope.results = result;
-		if( result.page < result.page_count )
+		if( result.page + 1 < result.page_count && result.page_count > 1 )
 			$scope.next_url = setQueryStringPage($location.absUrl(), result.page + 1);
 		if( result.page > 0 )
 			$scope.prev_url = setQueryStringPage($location.absUrl(), result.page - 1);
@@ -54,5 +54,19 @@ app.controller('NavController', ['$scope', '$window', '$cookieStore',
         $cookieStore.put('sponge_search', $scope.search_term);
         $window.location.href =
             "/search?search=" + $scope.search_term + "&county=" + $scope.selected_county_id + "&page=0";
+    }
+}]);
+
+app.controller('LoginController', ['$scope', '$window',
+    function($scope, $window) {
+    $scope.login = function(){
+		// Todo
+    }
+}]);
+
+app.controller('SignupController', ['$scope', '$window',
+    function($scope, $window) {
+    $scope.signup = function(){
+		// Todo
     }
 }]);
