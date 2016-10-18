9134b821d9077347f1fc43e5a628f0642d952935
ian luddy <ianluddy@gmail.com>
2016-10-18 21:03:39 +0100

    Constants

---
+++ angular_flask/constants.py
@@ -0,0 +1,7 @@
+# Strings
+
+EMAIL_NOT_FOUND = "Email address not registered"
+EMAIL_ADDRESS_TAKEN = "Email address already registered"
+PASSWORD_DOESNT_MATCH = "Password doesn't match"
+PASSWORD_NOT_VALID = "Password should be at least 8 characters long"
+EMAIL_NOT_VALID = "Email address not valid"
